13b91b6be50ad0e9a0deee0dc1fe97c72437fdaf
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-09-11 13:10:32 +0300

    change api keys

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=GEOCODEKEY&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibGF1cmF0ZWxsZXJ2byIsImEiOiJja2V5MGRra2IwMG0zMnRvZzB6NnpnZ3Y2In0.fp9dQL7oTpN4i0-T2Jii1w&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
