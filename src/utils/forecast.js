b41367398300cdc3f25d813f6026f1c78968b7f7
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-09-11 13:02:21 +0300

    change api keys

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+
+
+const forecast = (latitude, longitude, callback) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=FORECASTKEY&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location. ', undefined)
+        } else {
+            // console.log(body.current.weather_icons[0])
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
+        }
+    })
+}
+
+module.exports = forecast
